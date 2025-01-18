725acc3c86ce32e62e43e979210daa04218ef07b
TheSupremeTaco <maksymlike@gmail.com>
2025-01-17 20:49:26 -0800

    fnished last challenge

--- index.js
+++ index.js
@@ -180,7 +180,13 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
-
+let [firstGame, secGame, ...rest] = sortedGames;
+console.log(firstGame, secGame);
 // create a new element to hold the name of the top pledge game, then append it to the correct element
-
+let topPledgedGame = document.createElement("div");
+topPledgedGame.innerHTML = firstGame.name;
+firstGameContainer.append(topPledgedGame)
 // do the same for the runner up item
+let runnerPledgedGame = document.createElement("div");
+runnerPledgedGame.innerHTML = secGame.name;
+secondGameContainer.append(runnerPledgedGame)
